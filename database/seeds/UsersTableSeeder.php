5e408e1a9d282530063edaec8850f3beccfcc396
Jason Seah <jason@greenroom.com.my>
2018-03-11 16:30:34 +0800

    Add default user seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'Alex Tang';
+        $user->email = 'alex@gmail.com';
+        $user->password = bcrypt('111111');
+        $user->save();
+    }
+}
